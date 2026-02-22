5bf013815dcfd60cb5eea848ce2fa0e1c5dfa429
tekla janadze <tekla.janadze5@gmail.com>
2026-02-22 15:26:33 +0400

    Delete level 49 directory

--- level 49/homework/test.js
+++
@@ -1,9 +0,0 @@
-const changeBgColor = document.getElementById('bgcolor')
-
-changeBgColor.addEventListener('click',()=> {
-    const r = (Math.floor(Math.random())*256)
-    const g = (Math.floor(Math.random())*256)
-    const b = (Math.floor(Math.random())*256)
-
-    document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-})
