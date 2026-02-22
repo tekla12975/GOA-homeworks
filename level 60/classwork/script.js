759e4f3aab93f40db206049af2a42c3e0fcb6c12
tekla janadze <tekla.janadze5@gmail.com>
2026-02-22 15:23:04 +0400

    Delete level 60 directory

--- level 60/classwork/script.js
+++
@@ -1,9 +0,0 @@
-const form = document-getElementById(' formElement');
-const table = document-getElementById('table');
-const tr = document. getElementByld('tbodyTrElement');
-const products = [];
-
-
-
-
-// parsInt
